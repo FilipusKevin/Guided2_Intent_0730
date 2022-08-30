835ef3ab65c49f46a10fc842288ec4bb056ac782
FilipusKevin <filipuskevin9@gmail.com>
2022-08-30 23:51:49 +0700

    Guided 2

---
+++ app/src/main/java/com/GD2/guided2_intent_0730/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.GD2.guided2_intent_0730
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val btnNext = findViewById<Button>(R.id.btnNext)
+
+        btnNext.setOnClickListener {
+            val intent = Intent(this, NextActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
+
